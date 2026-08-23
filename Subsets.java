9cf09f0c43bae1a42ee82355fe0a5dee48e2f53d
Lavkush Nishad <lovekushnishad003@gmail.com>
2026-08-23 08:14:43 +0530

    subsets of a string

---
+++ Subsets.java
@@ -0,0 +1,29 @@
+
+//Find and print all subsets of a given string
+//  string="abc" => output= "", "a", "b", "c", "ab", "ac", "bc", "abc"
+
+public class Subsets{
+    public static void findSubsets(String str, String ans, int i){
+        //base case
+        if(i==str.length()){
+            if(ans.length()==0){
+                System.out.println("null");
+
+            }else{
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        //Yes choice
+        findSubsets(str, ans+str.charAt(i),i+1);
+        //No Choice
+
+        findSubsets(str, ans, i+1);
+    }
+    public static void main(String[] args){
+   String str="abc";
+   findSubsets(str, "", 0);
+
+    }
+}
